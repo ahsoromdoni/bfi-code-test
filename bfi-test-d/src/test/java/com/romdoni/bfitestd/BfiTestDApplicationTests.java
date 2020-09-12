0bf9376c0fbeda01cb7d9bde542be4bd5d7122dd
ASRomdon <dablegcoblan@gmail.com>
2020-09-12 12:38:55 +0700

    initial project test d

---
+++ bfi-test-d/src/test/java/com/romdoni/bfitestd/BfiTestDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romdoni.bfitestd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfiTestDApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
