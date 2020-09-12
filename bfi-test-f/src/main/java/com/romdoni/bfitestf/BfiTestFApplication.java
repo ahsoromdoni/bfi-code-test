0d8277dfca1df34ff75e3db5bafd958959cdc8c8
ASRomdon <dablegcoblan@gmail.com>
2020-09-12 14:29:53 +0700

    initial projecttest f

---
+++ bfi-test-f/src/main/java/com/romdoni/bfitestf/BfiTestFApplication.java
@@ -0,0 +1,13 @@
+package com.romdoni.bfitestf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfiTestFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfiTestFApplication.class, args);
+	}
+
+}
