61a5a0692f47bfbcf95a1b627eb838a2d98be702
ASRomdon <dablegcoblan@gmail.com>
2020-09-12 13:54:19 +0700

    initial project test e

---
+++ bfi-test-e/src/main/java/com/romdoni/bfiteste/BfiTestEApplication.java
@@ -0,0 +1,13 @@
+package com.romdoni.bfiteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfiTestEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfiTestEApplication.class, args);
+	}
+
+}
---
+++ bfi-test-e/src/test/java/com/romdoni/bfiteste/BfiTestEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romdoni.bfiteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfiTestEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
